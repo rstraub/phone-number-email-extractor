609e4bfd8c3ae0b62b79c96d91b1d590ab86b742
Roy Straub <roy.straub@codesquad.nl>
2021-02-06 11:22:41 +0100

    trying out some setup

--- core/phoneAndEmail.py
+++ core/phoneAndEmail.py
@@ -1,13 +1,16 @@
 import re
 
-phoneRegex = re.compile(r'''(
+phoneRegex = re.compile(
+    r"""(
     (\d{3}|\(\d{3}\))?  # Area Code
     (\s|-|\.)?          # Separator
     (\d{3})             # First three digits
     (\s|-|\.)?          # Separator
     (\d{4})             # Last four digits
     (\s*(ext|x|ext.)\s*(\d{2,5}))?
-)''', re.VERBOSE)
+)""",
+    re.VERBOSE,
+)
 
 # TODO: Create email regex
 
