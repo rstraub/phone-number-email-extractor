851ef7714f8996afd1cdc53391135b7b3c0e8906
Roy Straub <roy.straub@codesquad.nl>
2021-02-06 11:14:09 +0100

    added the regex for phone numbers

--- core/phoneAndEmail.py
+++ core/phoneAndEmail.py
@@ -1,6 +1,13 @@
 import re
 
-# TODO: Create phone regex
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?  # Area Code
+    (\s|-|\.)?          # Separator
+    (\d{3})             # First three digits
+    (\s|-|\.)?          # Separator
+    (\d{4})             # Last four digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?
+)''', re.VERBOSE)
 
 # TODO: Create email regex
 
